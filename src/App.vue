1c8785a8ab87ccc7a4aea3754321dd174f0befe4
Olivia Axelsson <olivia.axelsson@gmail.com>
2021-03-14 16:01:14 +0100

    All dice
    Co-authored-by: Lars Johansson <Johansson@carismar.com>

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="discarded">
+      <Die 
+        v-for="die in discardedDice" 
+        :key="die.color"
+        :color="die.color" 
+        :dot-color="die.dotColor"
+        :static-value="die.value" 
+        :roll="die.roll" 
+        @clicked="returnDiscardedDice(die)" />
+    </div>
+    <div class="table">
+      <Die 
+        v-for="die in dice" 
+        :key="die.color" 
+        :color="die.color" 
+        :dot-color="die.dotColor"
+        :roll="die.roll" 
+        @clicked="chooseDice(die)" 
+        @changed="die.value = $event" />
+    </div>
+    <div class="chosen">
+      <Die 
+        v-for="die in chosenDice" 
+        :key="die.color" 
+        :color="die.color"
+        :dot-color="die.dotColor"
+        :static-value="die.value" 
+        :roll="die.roll" 
+        @clicked="returnChosenDice(die)" />
+    </div>
+    <div class="controls">
+      <button class="button" @click="roll">Roll Dice</button>
+      <button class="button" @click="reset">Reset</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Die from './components/Die.vue';
+import {cleverHochDrei} from './settings.js';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Die
+  },
+   data: () => ({
+    dice: [...cleverHochDrei],
+    discardedDice: [],
+    chosenDice: [],
+    diceRollSound: new Audio(require('./assets/dice-roll.mp3'))
+  }),
+  mounted() {
+  },
+  methods: {
+    chooseDice(die) {
+      this.chosenDice.push(die);
+      if (this.chosenDice.length === 3) {
+        this.discardedDice = this.discardedDice.concat(this.dice.filter(item => item.color != die.color));
+        this.dice = [];
+      } else {
+        this.discardedDice = this.discardedDice.concat(this.dice.filter(item => item.value < die.value));
+        this.dice = this.dice.filter(item => item.value >= die.value && item.color != die.color);
+      }
+    },
+    returnDiscardedDice(die) {
+      this.dice.push(die);
+      this.discardedDice = this.discardedDice.filter(item => item.color != die.color);
+    },
+    returnChosenDice(die) {
+      this.dice.push(die);
+      this.chosenDice = this.chosenDice.filter(item => item.color != die.color);
+    },
+    roll() {
+      this.dice.map(i => {
+        i.roll++
+        return i;
+      });
+      this.diceRollSound.play();
+    },
+    reset() {
+      this.dice = [...cleverHochDrei];
+      this.discardedDice = [];
+      this.chosenDice = [];
+    }
   }
 }
 </script>
@@ -23,6 +95,60 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+  display:flex;
+  justify-content: center;
+  flex-wrap:wrap;
+}
+.discarded {
+  width:60%;
+  background: #ccc;
+  height: 25vh;
+  border-radius:50%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom:3vh;
+}
+.table {
+  width:60%;
+  height: 50vh;
+  background: #d4b992;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 40px;
+}
+.chosen {
+  width: 15%;
+  margin-left:5%;
+  box-shadow: 0 0 15px -7px rgba(0, 0, 0, 0.5);
+  border-radius: 40px;
+  display:flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 35px 0 0;
+}
+.controls {
+  width:100%;
+  position: fixed;
+  left:0;
+  bottom:0;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  height:15vh;
+  background: #333;
+}
+.button {
+    background: #fc0;
+    font-size:25px;
+    border:none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    text-transform: uppercase;
+    font-weight: bold;
+    margin: 0 20px;
 }
 </style>
---
+++ src/components/Die.vue
@@ -0,0 +1,85 @@
+<template>
+  <button class="die" :class="'die--'+ dieValue" :style="'background-color:' + color + ';color:'+ dotColor" @click="$emit('clicked')">
+    <div v-for="n in dieValue" :key="n" class="dot" :style="'background-color:' + dotColor"></div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Die',
+  props: {
+    roll: {
+      type: Number,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    staticValue: {
+      type: Number,
+    },
+    dotColor: {
+      type: String,
+      required: true
+    }
+  } ,
+  data: () => ({
+    value: 0
+  }),
+  watch: {
+    roll() {
+      this.value = Math.floor(Math.random() * 6) + 1;
+      this.$emit('changed', this.value);
+    } 
+  },
+  computed: {
+    dieValue() {
+      return this.staticValue || this.value;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.die {
+  width: 100px;
+  height: 100px;
+  border:none;
+  border-radius: 20px;
+  font-weight:bold;
+  font-size:50px;
+  box-shadow: 0 0 15px -7px rgba(0, 0, 0, 0.5);
+  margin: 10px;
+  padding:1%;
+  display:flex;
+}
+.die--1 {
+  align-items: center;
+  justify-content: center;
+}
+.die--2, 
+.die--3 {
+  display:flex;
+  justify-content: space-between;
+}
+.die--2 .dot:last-child,
+.die--3 .dot:last-child {
+  align-self: flex-end;
+}
+.die--3 .dot:nth-of-type(2) {
+  align-self: center;
+}
+.die--4 {
+  
+}
+.die--2 {
+
+}
+.dot {
+  width: 25%; 
+  height: 25%;
+  border-radius: 50%;
+}
+</style>
